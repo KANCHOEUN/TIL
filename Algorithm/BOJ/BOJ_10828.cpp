60338dbfccb559713f7bc709346d90b1c954b811
KANCHOEUN <kancho1216@naver.com>
2019-04-29 05:54:04 +0900

    2019 4월 4주차 (4)

---
+++ Algorithm/BOJ/BOJ_10828.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#define MAX 10000
+using namespace std;
+
+// ERROR 미완성
+
+class Stack {
+private:
+	int s[MAX];
+	int top;
+
+public:
+	Stack() {
+		top = -1;
+	}
+
+	int Size() const {
+		return top + 1;
+	}
+
+	bool IsEmpty() const {
+		if (top == -1) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	int Top() const {
+		return s[top];
+	}
+
+	void Push(int x) {
+		if (top == (MAX - 1)) {
+			cout << "Stack Overflow" << endl;
+			return;
+		}
+		s[top++] = x;
+	}
+
+	void Pop() {
+		if (IsEmpty() == true) {
+			cout << "-1" << endl;
+		}
+		else {
+			cout << s[top--] << endl;
+		}
+	}
+};
+
+int main() {
+	Stack s;
+	int N;
+	string str;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> str;
+
+		if (!str.compare("push")) {
+			int num;
+			cin >> num;
+			s.Push(num);
+		}
+		else if (!str.compare("pop")) {
+			s.Pop();
+		}
+		else if (!str.compare("size")) {
+			cout << s.Size() << endl;
+		}
+		else if (!str.compare("empty")) {
+			cout << s.IsEmpty() << endl;
+		}
+		else if (!str.compare("top")) {
+			cout << s.Top() << endl;
+		}
+		else {
+			cout << "Not available" << endl;
+		}
+	}
+
+	return 0;
+}
