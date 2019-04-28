2aae2caaf7da792f1ff6b92cd7a26c12b5b4eb12
KANCHOEUN <kancho1216@naver.com>
2019-04-29 06:13:23 +0900

    2019 4월 4주차 (5)

---
+++ Algorithm/BOJ/BOJ_1427.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <String>
+using namespace std;
+
+// 컴파일 에러??
+
+void Swap(int &a, int &b)
+{
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+
+void BubbleSort(int *(&arr), int len)
+{
+	for (int i = 0; i < len - 1; i++)
+	{
+		for (int j = 1; j < len - i; j++)
+		{
+			if (arr[j] > arr[j - 1])
+			{
+				Swap(arr[j], arr[j - 1]);
+			}
+		}
+	}
+}
+
+int main()
+{
+	string str;
+	cin >> str;
+	int len = str.length();
+	int * arr = new int[len];
+
+	for (int i = 0; i < len; i++)
+	{
+		arr[i] = str[i] - '0';
+	}
+
+	BubbleSort(arr, len);
+
+	for (int j = 0; j < len; j++)
+	{
+		cout << arr[j];
+	}
+
+  delete []arr;
+	return 0;
+}
