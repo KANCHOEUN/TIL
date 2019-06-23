993861159eee9cf0684b63541bb2eb870a7dcc54
KANCHOEUN <kancho1216@naver.com>
2019-06-24 02:46:46 +0900

    2019 6월 4주차 (4)

---
+++ Algorithm/BOJ/BOJ_3009.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int x[4];
+int y[4];
+
+int main() {
+	for (int i = 0; i < 3; i++) {
+		scanf("%d %d", &x[i], &y[i]);
+	}
+
+	if (x[0] == x[1]) {
+		x[3] = x[2];
+	}
+	else if (x[0] == x[2]) {
+		x[3] = x[1];
+	}
+	else {
+		x[3] = x[0];
+	}
+
+	if (y[0] == y[1]) {
+		y[3] = y[2];
+	}
+	else if (y[0] == y[2]) {
+		y[3] = y[1];
+	}
+	else {
+		y[3] = y[0];
+	}
+
+	printf("%d %d\n", x[3], y[3]);
+
+	return 0;
+}
